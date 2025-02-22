0c7adea423233d21ce7259a77e20d1d521679d09
Yojan Gonzalez <152090729+ygon28@users.noreply.github.com>
2025-02-21 19:26:53 -0700

    Update casa_8.py

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,14 @@
+import pandas as pd
+df = pd.read_csv('./big-mac-full-index.csv')
+def iterate_apply(row):
+    return row[2]
 
+result = df.apply(iterate_apply, axis = 1)
+count = 0
+for res in result:
+    if count < 10:
+        print(res)
+        count += 1
+
+for index, row in df.iterrows():
+    print(f'{row[2]}')
